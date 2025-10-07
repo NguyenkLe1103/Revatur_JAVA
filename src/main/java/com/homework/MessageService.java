99b3d2afb1b4ea40d56f43c6223ec984174158a4
NguyenkLe1103 <nguyenkle0311@gmail.com>
2025-10-07 07:02:51 -0500

    change to setter injection

--- src/main/java/com/homework/MessageService.java
+++ src/main/java/com/homework/MessageService.java
@@ -2,15 +2,14 @@ package com.homework;
 
 public class MessageService {
     private String message;
-public MessageService(String message){
-    this.message = message;
+public MessageService(){}
 
+public void setMessage(String message){
+    this.message = message;
 }   
 public void printMessage(){
     System.out.println("Your Message: " + message);
 }
-public void setMessage(String message){
-    this.message = message;
-}
+
 
 }
