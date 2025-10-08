80e97dbd15c75a39848a6b0e54b4284bfc4c09d5
NguyenkLe1103 <nguyenkle0311@gmail.com>
2025-10-08 07:51:40 -0500

    Update App.java

--- src/main/java/com/ecom/annotation/App.java
+++ src/main/java/com/ecom/annotation/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        // Pick ANY one of your services to run (adjust to what you created)
+        
         CheckoutService svc = ctx.getBean(CheckoutService.class);
         svc.checkout("ctor@example.com", 300.00);
     }
